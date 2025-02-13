06c203d9d46b7c5db9781389ebae2db1c165715d
Juliya <juliya@MacBookAir.lan>
2025-02-13 16:28:09 +0300

    Type-functions.js

---
+++ lesson-3/Type-functions.js
@@ -0,0 +1,15 @@
+// Типы функций
+
+// В JavaScript существует три основных типа функций:
+// ⎯ Объявляемая функция (function declaration);
+// ⎯ Функциональное выражение (function expression);
+// ⎯ Стрелочная функция (arrow function, ES6).
+
+// И ещёодин специфичныйтип:
+// ⎯ Именованное функциональное выражение (named function expression, NFE).
+
+// Кроме этого есть ещёнекоторые «состояния» функций:
+// ⎯ Анонимная функция (anonymous function);
+// ⎯ Callback-функция (callback function);
+// ⎯ Функция-конструктор (constructor function);
+// ⎯ Функция высшего порядка (higher-order function).
